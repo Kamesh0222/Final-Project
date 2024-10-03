6aa91d44221c421f97e8aad764c9203ec4e54a7a
Kamesh0222 <kameshwaranm@presidio.com>
2024-10-03 12:49:07 +0530

    Specialties update

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Specialties from "../components/Specialties";
 
 const App = () => {
   return (
     <>
       <Hero />
+      <Specialties />
     </>
   );
 };
